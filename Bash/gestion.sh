db91d9ea95000b60f47f4d87dde959dabe9b0817
Ahmed <ahmedlemrabett81@gmail.com>
2026-05-12 10:22:28 +0000

    Ajouter des scripts Bash

---
+++ Bash/gestion.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "====== Projet Linux ======"
+
+echo "Nom d'utilisateur:"
+whoami
+
+echo "Date actuelle:"
+date
+
+echo  "Contenu du dossier :"
+ls
+
+echo "Creation du dossier sauvegarde..."
+mkdir -p sauvegarde
+
+echo "Projet terminé"
+
+
+ 
---
+++ Bash/systéme.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "=== INFOS SYSTEME ==="
+echo ""
+
+echo "Dossier actuel:"
+pwd
+echo ""
+
+echo "Utilisateur:"
+whoami
+echo ""
+
+echo "Date et heure:"
+date
+echo ""
+
+echo "fichiers ici:"
+ls
+echo ""
+
+echo "espace disque:"
+df -h
+echo ""
+
+echo "============================"
